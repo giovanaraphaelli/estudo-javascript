56b33d82950009118c95aa4064e56c01c7bbfd71
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2022-03-24 11:02:08 -0300

    e vamos de estudar

--- ativar-e-desativar-classe.js
+++ ativar-e-desativar-classe.js
@@ -1,9 +1,10 @@
-const botao = document.querySelector(".botao");
+const botao = document.querySelector(".botao"); // peguei o elemento
 console.log(botao);
 
+// criei a função
 function mostrar() {
-  const texto = document.querySelector(".texto");
-  texto.classList.toggle("ativar");
+  const texto = document.querySelector(".texto"); // peguei o elemento que vai mudar
+  texto.classList.toggle("ativar"); // add e removi através do toggle a classe
 }
 
-botao.addEventListener("click", mostrar);
+botao.addEventListener("click", mostrar); // adicionei o evento de click para a função
